4404043fec0ac628411aa756fa3985f9c1367d3b
Gauravst <gauravsinghtangariya1@gmail.com>
2024-10-17 08:12:25 +0530

    favicon changed and sitemap updated

--- components/utils/dashboard/header.php
+++ components/utils/dashboard/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="robots" content="noindex, nofollow">
-    <link rel="icon" href="../assets/favicon/favicon.ico" />
+    <link rel="icon" href="../favicon.ico" />
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
--- components/utils/header.php
+++ components/utils/header.php
@@ -8,7 +8,7 @@ $ini = parse_ini_file($iniPath, true);
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="assets/favicon/favicon.ico" />
+    <link rel="icon" href="favicon.ico" />
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
